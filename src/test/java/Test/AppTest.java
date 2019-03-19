3ff94b857989e4db8640fdc3815ad26d90c2a41e
unknown <alexandragodan@gmail.com>
2019-03-20 01:14:13 +0200

    Initial commit

---
+++ src/test/java/Test/AppTest.java
@@ -0,0 +1,19 @@
+package Test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddStudent1()
+    {
+        
+    }
+ @Test
+    public void testAddStudent2()
+    {
+        assertTrue( true );
+    }
+
+}
